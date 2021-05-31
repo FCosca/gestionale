d5965a4b8b78f3e981bed6aa6c4275c3fb125fb1
FCosca <francecosca@gmail.com>
2021-05-31 02:45:01 +0200

    FE: add method getDatiAll(prodotti)

--- GestionaleFE/gestionale/src/Components/ProdottiComponent.js
+++ GestionaleFE/gestionale/src/Components/ProdottiComponent.js
@@ -5,23 +5,13 @@ import React, { Component, useState } from 'react'
   
 
 const Prodotti = ({ prodotti }) => {
-    const [name, setName] = useState("")
-
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        <a></a>
-    }
+    
     
     return (
         <div id='corpo'>
             <h1>Prodotti</h1>
             {/* {prodotti.id} */}
-            <form onSubmit={handleSubmit}>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1"  >nome</span>
-                    <input type="text" class="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" value={name} onChange={e => setName(e.target.value)}/>
-                </div>
-            </form>
+            
         
             <table class="table">
                 <thead>
@@ -32,7 +22,7 @@ const Prodotti = ({ prodotti }) => {
                         <th scope="col">Giacenza</th>
                     </tr>
                 </thead>
-                {prodotti.map((prodotto) => (
+                {prodotti && prodotti.length > 0 && prodotti.map((prodotto) => (
                 <tbody key={prodotto.id}>
                     <tr>
                         <th scope="row">{prodotto.id}</th>
--- GestionaleFE/gestionale/src/services/ProdottiSerivce.js
+++ GestionaleFE/gestionale/src/services/ProdottiSerivce.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Prodotti from '../../src/Components/ProdottiComponent'
+import ProdottiCom from '../../src/Components/ProdottiComponent'
 import config from '../config';
 
 
@@ -7,81 +7,26 @@ import config from '../config';
 
 function ProdottiFetch (){
   const [prodotti,setProdotti] = useState([])
+  
+  
 
   useEffect(() => {
     const getDatiAll = async() =>{
       const res = await fetch(`${config.api}prodotti/findAll`)
       const data = await res.json()
-      .then (data => setProdotti(data))
+      .then (data => setProdotti(data), console.log(prodotti))
       .catch(error => alert(error='error loading the API'))
+      
     }
-    //getDatiAll();
-    //GetProdottiByNome("giacca").then(setProdotti)
-    //GetProdottiById(1).then(setProdotti)
-    //GetProdottiOrderByCres().then(setProdotti)
-    GetProdottiOrderByDecre().then(setProdotti)
+    getDatiAll();    
   },[]) 
 
-  async function GetProdottiById(id){
-    const res = await fetch(`${config.api}prodotti/findByID?id=${id}`,{
-      method: 'GET'
-    })
-    const data = await res.json();
-    if (res.status >= 400) {
-      console.warn("ERROR api");
-      throw new Error(data.message);
-    }
-    return data;
-    
-    
-  };
 
-  async function GetProdottiByNome(nome){
-    const res = await fetch(`${config.api}prodotti/findByNome?nome=${nome}`,{
-      method: 'GET'
-    })
-    const data = await res.json();
-    if (res.status >= 400) {
-      console.warn("ERROR api");
-      throw new Error(data.message);
-    }
-    return data;   
-    
-  };
-
-  async function GetProdottiOrderByCres(){
-    const res = await fetch(`${config.api}prodotti/FindGiacCres`,{
-      method: 'GET'
-    })
-    const data = await res.json();
-    if (res.status >= 400) {
-      console.warn("ERROR api");
-      throw new Error(data.message);
-    }
-    return data;
-
-  };
-
-  async function GetProdottiOrderByDecre(){
-    const res = await fetch(`${config.api}prodotti/FindGiacDecre`,{
-      method: 'GET'
-    })
-    const data = await res.json();
-    if (res.status >= 400) {
-      console.warn("ERROR api");
-      throw new Error(data.message);
-    }
-    return data;
-
-  };
-
-
-
-   return (
-     prodotti ? <Prodotti prodotti={prodotti}/>  : <></>
-    
- )
+  return(
+    prodotti ? <ProdottiCom prodotti={prodotti} setProdotti={setProdotti} />  : <></>
+    ) 
   }
+
 
 
 export default ProdottiFetch
