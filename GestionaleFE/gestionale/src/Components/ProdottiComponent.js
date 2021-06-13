3605570525187c43fa188fdb68db65f764115620
FCosca <francecosca@gmail.com>
2021-06-13 02:24:05 +0200

    add model for aggiunta prodotti

--- GestionaleFE/gestionale/src/Components/ProdottiComponent.js
+++ GestionaleFE/gestionale/src/Components/ProdottiComponent.js
@@ -1,5 +1,7 @@
 import React, { Component, useState } from 'react'
 import { Link } from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 
 
 
@@ -12,6 +14,12 @@ const Prodotti = ({ prodotti, GetProdottiByNome, GetProdottiOrderByDecre, GetPro
     const [descrizioneC, setDescrizioneC] = useState("")
     const [giacenzaC, setGiacenzaC] = useState(0)
     const [idC, setIdC] = useState(0)
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const [show2, setShow2] = useState(false);
+    const handleClose2 = () => setShow2(false);
+    const handleShow2 = () => setShow2(true);     
 
 
     const handleSubmit = (e) =>{
@@ -21,14 +29,14 @@ const Prodotti = ({ prodotti, GetProdottiByNome, GetProdottiOrderByDecre, GetPro
     }
 
     const finById =(e) =>{
-        e.preventDefault();
+        
         console.log(idC)
         GetProdottiById(idC)
 
     }
 
     function Insert(e){
-        e.preventDefault();
+        // e.preventDefault();
         console.log(nameC)
         console.log(descrizioneC)
         console.log(giacenzaC)
@@ -62,6 +70,26 @@ const Prodotti = ({ prodotti, GetProdottiByNome, GetProdottiOrderByDecre, GetPro
         DeleteProdotto(id)
     }
 
+    function chiama(){
+        handleShow()
+
+    }
+
+    function chiama2(){
+        Insert()
+        handleClose() 
+    }
+
+    function chiama3(){
+        handleShow2()
+
+    }
+
+    function chiama4(){
+        finById()
+        handleClose2() 
+    }
+
 
     
     
@@ -77,20 +105,22 @@ const Prodotti = ({ prodotti, GetProdottiByNome, GetProdottiOrderByDecre, GetPro
                 </div>
             </form>
 
-            <form onSubmit={finById}>
+            {/* <form onSubmit={finById}>
                 <div className="input-group mb-3">
                     <span className="input-group-text" id="basic-addon1"  >ID</span>
                     <input type="text" className="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" value={idC} onChange={e => setIdC(e.target.value)}/>
                     <input type="submit" value="Submit" />
                 </div>
-            </form>
+            </form> */}
+            <td><Button variant="primary" onClick={chiama3}>Cerca Per ID <i class="fas fa-search"></i></Button></td>
 
-            <form onSubmit={Insert}>
+            {/* <form onSubmit={Insert}>
                 <input type="text" className="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" value={nameC} onChange={e => setNameC(e.target.value)}/>
                 <input type="text" className="form-control" placeholder="Descrizione" aria-label="Nome" aria-describedby="basic-addon1" value={descrizioneC} onChange={e => setDescrizioneC(e.target.value)}/>
                 <input type="text" className="form-control" placeholder="Giacenza" aria-label="Nome" aria-describedby="basic-addon1" value={giacenzaC} onChange={e => setGiacenzaC(e.target.value)}/>
                 <input type="submit" value="Submit" />
-            </form>
+            </form> */}
+            <td><Button variant="primary" onClick={chiama}>Aggiungi Prodotto <i class="fas fa-plus"></i></Button></td>
         
             <table class="table">
                 <thead>
@@ -120,6 +150,46 @@ const Prodotti = ({ prodotti, GetProdottiByNome, GetProdottiOrderByDecre, GetPro
                 
                 ))}
             </table>
+
+            <Modal show={show} onHide={handleClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                  
+                    <input type="text" className="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" value={nameC} onChange={e => setNameC(e.target.value)}/>
+                    <input type="text" className="form-control" placeholder="Descrizione" aria-label="Nome" aria-describedby="basic-addon1" value={descrizioneC} onChange={e => setDescrizioneC(e.target.value)}/>
+                    <input type="text" className="form-control" placeholder="Giacenza" aria-label="Nome" aria-describedby="basic-addon1" value={giacenzaC} onChange={e => setGiacenzaC(e.target.value)}/>
+                            
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                      Close
+                    </Button>
+                    <Button variant="primary" onClick={chiama2}>
+                      Save Changes
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+
+                <Modal show={show2} onHide={handleClose2}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                  
+                    <input type="text" className="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" value={idC} onChange={e => setIdC(e.target.value)}/> 
+                            
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose2}>
+                      Close
+                    </Button>
+                    <Button variant="primary" onClick={chiama4}>
+                      Save Changes
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
         </div>
 
         
